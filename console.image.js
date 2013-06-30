b929cc1220a14af9775bf1aa4cd4f1bc3982611b
Adrian Cooney <cooney.adrian@gmail.com>
2013-06-30 16:39:37 +0100

    Fixing scale on console.image

--- console.image.js
+++ console.image.js
@@ -164,7 +164,7 @@
 
 		img.onload = function() {
 			var dim = getBox(this.width * scale, this.height * scale);
-			console.log("%c" + dim.string, dim.style + "background: url(" + url + "); color: transparent;");
+			console.log("%c" + dim.string, dim.style + "background: url(" + url + "); background-size: " + (this.width * scale) + "px " + (this.height * scale) + "px; color: transparent;");
 		};
 
 		img.src = url;
